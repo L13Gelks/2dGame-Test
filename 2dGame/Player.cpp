6dd0f988a866f37448563e4c78952693bb65cf11
Yadir Rojas Delgado <yadirgerardo@gmail.com>
2021-04-02 14:15:28 -0600

    classes modfified in a sexy manner

---
+++ 2dGame/Player.cpp
@@ -0,0 +1,107 @@
+#include "Player.h"
+
+Player::Player(const sf::Vector2f& pos)
+    :
+    pos(pos)
+{
+    sprite.setScale(0.2, 0.2);
+    animations[int(AnimationIndex::Idle)] = PlayerAnimation(0, 16, "sprite/Idle (", "1", ").png");
+    animations[int(AnimationIndex::WalkingRight)] = PlayerAnimation(1, 20, "sprite/Walk (", "1", ").png");
+    animations[int(AnimationIndex::WalkingLeft)] = PlayerAnimation(2, 20, "sprite/Walk (", "1", ").png");
+    animations[int(AnimationIndex::RunRight)] = PlayerAnimation(1, 20, "sprite/Run (", "1", ").png");
+    animations[int(AnimationIndex::RunLeft)] = PlayerAnimation(2, 20, "sprite/Run (", "1", ").png");
+    animations[int(AnimationIndex::Jump)] = PlayerAnimation(0, 30, "sprite/Jump (", "1", ").png");
+}
+
+void Player::Draw(sf::RenderTarget& rt) const
+{
+    rt.draw(sprite);
+}
+
+void Player::SetDirection(sf::Vector2f& dir)
+{
+    if (jump) {
+        time += 1;
+
+        if (time < jump_time / 2) { dir.y -= 1; }
+        else { dir.y += 1; }
+
+        if (!walking && !running) {
+            if (faceDir) { dir.x = -0.2; }
+            else { dir.x = 0.2; }
+            dir.y *= 0.5;
+        }
+        else if (walking) {
+            if (faceDir) { dir.x = -0.4; }
+            else { dir.x = 0.4; }
+            dir.y *= 0.6;
+        }
+        else if (running) {
+            if (faceDir) { dir.x = -1.4; }
+            else { dir.x = 1.4; }
+            dir.y *= 0.8;
+        }
+
+
+        if (time > jump_time) {
+            jump = false;
+            time = 0;
+            vel.y = 0;
+            jump_time = 90;
+        }
+    }
+    if (shift)
+    {
+        speed = 150.0f;
+    }
+    else {
+        speed = 70.0f;
+    }
+    vel = dir * speed;
+    if (!jump && sprite.getPosition().y < 400) {
+        dir.y = 2;
+        dir.x = 0;
+        vel = dir * speed;
+    }
+    else if (dir.y != 0) {
+        curAnimation = AnimationIndex::Jump;
+    }
+    else if (dir.x == 0.0f && dir.y == 0.0f) {
+        curAnimation = AnimationIndex::Idle;
+        walking = false;
+        running = false;
+    }
+    else if (dir.x > 0 && shift) {
+        running = true;
+        faceDir = 0;
+        curAnimation = AnimationIndex::RunRight;
+    }
+    else if (dir.x < 0 && shift) {
+        running = true;
+        faceDir = 1;
+        curAnimation = AnimationIndex::RunLeft;
+    }
+    else if (dir.x > 0) {
+        walking = true;
+        faceDir = 0;
+        curAnimation = AnimationIndex::WalkingRight;
+    }
+    else if (dir.x < 0) {
+        walking = true;
+        faceDir = 1;
+        curAnimation = AnimationIndex::WalkingLeft;
+    }
+    if (sprite.getPosition().y > 400) {
+        pos.y = 400;
+        sprite.setPosition(pos);
+        vel.y = 0;
+    }
+}
+
+void Player::Update(float dt)
+{
+    pos += vel * dt;
+    animations[int(curAnimation)].Update(dt);
+    animations[int(curAnimation)].ApplyToSprite(sprite);
+    sprite.setPosition(pos);
+}
--- 2dGame/PlayerAnimation.h
+++ 2dGame/PlayerAnimation.h
@@ -4,53 +4,11 @@ class PlayerAnimation
 {
     public:
         PlayerAnimation() = default;
-        PlayerAnimation(int value_in, int frames_in, std::string str_1, std::string str_2, std::string str_3)
-        {
-            value = value_in;
-            str1 = str_1;
-            str2 = str_2;
-            str3 = str_3;
-            nFrames = frames_in;
-            std::string url = str1 + str2 + str3;
-            texture.loadFromFile(url);
-            for (int i = 0; i < nFrames; i++) {
-                str2 = std::to_string((i + 1));
-                url = str1 + str2 + str3;
-                frames[i] = { url };
-            }
-        }
-
-        void ApplyToSprite(sf::Sprite& s) {
-
-            texture.loadFromFile(frames[iFrame]);
-            s.setTexture(texture);
-            s.setOrigin({ s.getLocalBounds().width / 2, 0 });
-
-            if (value == 2) {
-                //left
-
-                s.setScale({ -0.2, 0.2 });
-            }
-            else if (value == 1) {
-                //right
-                s.setScale({ 0.2, 0.2 });
-
-            }
-            else {
-
-            }
-        }
-        void Update(float dt) {
-            time += dt;
-            while (time >= holdTime) {
-                time -= holdTime;
-                Advance();
-            }
-        }
+        PlayerAnimation(int value_in, int frames_in, std::string str_1, std::string str_2, std::string str_3);
+        void ApplyToSprite(sf::Sprite& s);
+        void Update(float dt);
     private:
-        void Advance() {
-            if (++iFrame >= nFrames) { iFrame = 0; }
-        }
+        void Advance();
     private:
         sf::Texture texture;
         int nFrames = 0;
