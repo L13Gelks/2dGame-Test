a3ade1b609868d6b7ac7053faceea60c19d46adf
Yadir Rojas Delgado <yadirgerardo@gmail.com>
2021-04-20 11:41:20 -0600

    added dynamic world

--- 2dGame/Game.cpp
+++ 2dGame/Game.cpp
@@ -9,6 +9,46 @@ Game::~Game()
 {
 }
 
+void Game::WorldCreator()
+{
+    sf::Vector2f vec1 = { 0,0 };
+    sf::Vector2f vec2 = { 0.0f,0.0f };
+    float groundMult = 0.0f;
+    ground.reserve(50);
+    if (traveledDistance == 0.0f) {
+        vec1 = { 0,0 };
+        vec2 = { 9.0f,1.0f };
+        ground.emplace_back(2, vec1, vec2);
+        vec1 = { 0,670 };
+        vec2 = { 140.0f,1.0f };
+        ground.emplace_back(1, vec1, vec2);
+        vec1 = { 0,610 };
+        vec2 = { 14.0f,1.0f };
+        groundMult = 14.0f;
+        ground.emplace_back(0, vec1, vec2);
+        travelGroundCheckPoint = groundMult * 100;
+        bottomGroundIndex = 2;
+    }
+    else {
+        if (travelGroundCheckPoint == traveledDistance+screenWidth)
+        {
+            std::random_device rd;
+            std::mt19937 rng(rd());
+            std::uniform_int_distribution<int> xDist(50, 200);
+            std::uniform_int_distribution<int> xMult(1, 3);
+
+            groundMult = xMult(rng);
+
+            vec1 = { travelGroundCheckPoint,610 };
+            vec2 = { groundMult,1.0f };
+
+            ground.emplace_back(0, vec1, vec2);
+            travelGroundCheckPoint += groundMult * 100;
+            travelGroundCheckPoint += xDist(rng);
+        }
+    }
+}
+
 void Game::startGame()
 {
     // Create the main window
@@ -19,26 +59,17 @@ void Game::startGame()
         // Error handling...
     }
     window.setIcon(image.getSize().x, image.getSize().y, image.getPixelsPtr());
+    sf::View view(sf::FloatRect(0.f, 0.f, screenWidth, screenHeight));
+    view.setCenter(sf::Vector2f(screenWidth / 2, screenHeight / 2));
+    window.setView(view);
     Player fucker({ 200.0f,180.0f });
-    sf::Vector2f vec1 = { 0,610 };
-    sf::Vector2f vec2 = { 2.0f,1.0f };
-
-    Ground ground(0,vec1,vec2);
-    vec1 = { 700,590 };
-    vec2 = { 4.0f,1.0f };
-    Ground ground2(0,vec1, vec2);
-    vec1 = { 0,670 };
-    vec2 = { 14.0f,1.0f };
-    Ground water(1, vec1, vec2);
-    vec1 = { 0,0 };
-    vec2 = { 1.0f,1.0f };
-    Ground background(2, vec1, vec2);
     //dt
     auto tp = std::chrono::steady_clock::now();
     // Start the game loop
     window.setFramerateLimit(60);
     while (window.isOpen())
     {
+        WorldCreator();
         // get dt
         float dt;
         {
@@ -89,27 +120,31 @@ void Game::startGame()
             fucker.setHurtState(false);
         }
 
-        fucker.SetDirection(dir);
-        water.SetDirection();
-        ground.SetDirection();
 
-       
-        fucker.TestCollision(ground2.GetSize(), ground2.GetPosition(), 0);
-        fucker.TestCollision(water.GetSize(), water.GetPosition(), 1);
-        fucker.TestCollision(ground.GetSize(), ground.GetPosition(), 0);
+        fucker.SetDirection(dir);
+
         //update model
         fucker.Update(dt);
-        ground.Update(dt);
-        water.Update(dt);
         // Clear screen
         window.clear();
+        view.move(worldTravelSpeed, 0.0f);
+        window.setView(view);
         // Draw the sprite
-        background.Draw(window);
+        ground[0].Draw(window);
         fucker.Draw(window);
-        ground.Draw(window);
-        ground2.Draw(window);
-        water.Draw(window);
+        for (int i = 0; i < ground.size(); i++)
+        {
+            ground[i].SetDirection();
+            fucker.TestCollision(ground[i].GetSize(), ground[i].GetPosition(), ground[i].getType());
+            ground[i].Update(dt);
+            if (i > 1)
+            {
+                ground[i].Draw(window);
+            }
+        }
+        ground[1].Draw(window);
         // Update the window
         window.display();
+        traveledDistance += worldTravelSpeed;
     }
 }
